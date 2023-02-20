f45aaaef5913d0a8125e7673820956559229363f
Marks Junior <joaojuniormarques6@gmail.com>
2023-02-20 16:01:24 -0300

    criada teste impar e par

---
+++ PARIMPAR/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class PARIMPAR
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entre com um numero inteiro");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("PAR!");
+
+
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("IMPAR!");
+            }
+        }
+    }
+}
